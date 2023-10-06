a7c26053ae02bafefcac20656fbcf972ec9b5ad8
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-06 17:35:08 +0000

    Created about page

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return <div>
+        <h1>The about Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident sit laudantium repudiandae consequuntur alias doloremque dolorem harum, quasi maxime veniam animi dolores nemo, odit incidunt temporibus! Ipsam tempora fugit cumque?</p>
+    </div>
+}
+
+export default AboutPage;

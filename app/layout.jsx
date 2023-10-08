b47bbd08cbc5a6d2a7c45ff41430d41e98f41863
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-08 15:37:50 +0000

    Turned application into pwa

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,11 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      {/* <meta name="description" content="description of your project" /> */}
+      <meta name="theme-color" content="#000" />
+      {/* <title>Title of the project</title> */}
+      <link rel="manifest" href="/manifest.json" />
       <body className={poppins.className}>
         <Header />
-        <main className="container">
-          {children}
-        </main>
+        <main className="container">{children}</main>
       </body>
     </html>
   );

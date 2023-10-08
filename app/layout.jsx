05aef1cef7048f7746927dc6a127f6ac6bc31998
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-08 15:47:36 +0000

    Removed certain files

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta
         name="viewport"
